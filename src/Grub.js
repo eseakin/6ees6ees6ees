57f2534d27b2f2739420e44d0769509f28a96697
Eric Eakin <eseakin@gmail.com>
2016-09-19 12:11:03 -0700

    Working up through Honey bee.

--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,13 @@
 class Grub {
+	constructor(age, color) {
+
+		this.age = age || 0;
+		this.color = color || 'pink';
+		this.food = 'jelly';
+	}
+	eat() {
+		return 'Mmmmmmmmm jelly';
+	}
 };
 
 export default Grub;
